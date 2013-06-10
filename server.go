c58d9f8d75752d46d0da1dc53fedbcff1a9e9088
Anton Taraev <anti191@gmail.com>
2013-06-11 03:52:45 +0400

    Add server and GAE

---
+++ server.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+    "encoding/json"
+    "log"
+)
+
+func main() {
+    http.HandleFunc("/", hello)
+    http.HandleFunc("/object", object)
+    http.ListenAndServe(":8080", nil)
+}
+
+func object(w http.ResponseWriter, r *http.Request) {
+  fmt.Println(r.Method, r.URL, r.Proto)
+
+  type Obj struct {
+    Text string
+    Centstatus int
+    Id string
+    Level int
+  }
+  m := Obj{"Подмостки (д)", 1, "j7d39dc2-cd93-466c-8389-4d6b56f017cb", 6}
+  b, err := json.Marshal(m)
+  if err != nil {log.Fatal(err)}
+  fmt.Fprintf(w, "%s", b)
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+  fmt.Println(r.Method, r.URL, r.Proto)
+  fmt.Fprintf(w, "<h1>Hello, world</h1><br/><img src='http://golang.org/doc/gopher/frontpage.png'>")
+}
